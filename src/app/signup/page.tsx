1a1b4cc8287a8448594a9fbb1bee44f7a4c2c12c
Mankirat Singh <msingh2_be22@thapar.edu>
2023-08-11 23:30:27 +0530

    Sign up page basic structure added
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-
+import styles from "./page.module.css";
 
 export default function SignupPage() {
     
     return (
         <div>
             <h1>Signup</h1>
+            <h4>And be ready for your passwords to be managed hassle free.</h4>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" />
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" />
+                <button type="submit">Sign Up</button>
+            </form>
+
         </div>
     );
 
