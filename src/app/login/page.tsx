9e2e0fae03b573673e818bedd144869d3cb194eb
Mankirat Singh <msingh2_be22@thapar.edu>
2023-08-19 23:38:16 +0530

    Login route added
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import styles from './login.module.css'
+
+export default function login(){
+    return(
+        <div>
+            <h1>This</h1>
+            <h1>is</h1>
+            <h1>Login</h1>
+            <h1>Page</h1>
+        </div>
+    )
+}
